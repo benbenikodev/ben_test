f257cb43de976382d4764d6893a4b435c804f113
Benbeniko <Benbeniko@DESKTOP-9K1HRS7>
2016-12-18 18:53:19 +0200

    my second commit

--- smartup/smartup/Program.cs
+++ smartup/smartup/Program.cs
@@ -11,6 +11,8 @@ namespace smartup
         static void Main(string[] args)
         {
             Console.WriteLine("Hello smart-up");
+            Console.WriteLine("hello hello");
+
         }
     }
 }
